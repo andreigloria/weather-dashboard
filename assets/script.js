6e857352bf31c10b26d80d052b0639130be3339a
andreigloria@gmail.com <andreigloria@gmail.com>
2023-01-30 20:53:41 +0000

    script and readme updated

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var APIkey = "40358e0ff9440bfbb506d9580e0689e6";
+var city;
+fetch(queryURL)
+
+// $("#search-button").on(|"click", function());
+
+// var queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={40358e0ff9440bfbb506d9580e0689e6}."
+
+// $.ajax({
+    // url: queryURL,
+    // method: "get"
+// })
+
