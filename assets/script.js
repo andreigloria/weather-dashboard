988f75c1991e04be405bcc56f4ca7196d6a45795
andreigloria@gmail.com <andreigloria@gmail.com>
2023-01-31 22:05:29 +0000

    modified script and html

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,43 @@
 var APIkey = "40358e0ff9440bfbb506d9580e0689e6";
-var city;
-fetch(queryURL)
+
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=london&appid=" + APIKey;
+
+$.ajax({
+    url: queryURL,
+    method: "get"
+})
+
+.then(function(response) {
+    console.log(queryURL);
+    console.log(response);
+});
+
+
+// $("#search-button").on("click", function() {
+    // var city = $(this).attr("search-button");
+//     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid={40358e0ff9440bfbb506d9580e0689e6}";
+// });
+
+// fetch(queryURL);
+//  $.ajax({
+//     url: queryURL,
+//     method: "get"
+//  })
+ 
+//  .then(function(response) {
+//     console.log(response)
+//  });
+//     for (var i = 0; i < results.length; i++ ) {
+
+//     }
+//  }
+
+// });
+    
+
+// var APIkey = "40358e0ff9440bfbb506d9580e0689e6";
+// var city;
+// fetch(queryURL)
 
 // $("#search-button").on(|"click", function());
 
